04a06525b92b075961ec6916d108e6a051ab0407
Vittorio Adesso <adessovittorio8@gmail.com>
2021-11-29 19:56:35 +0100

    Fase II

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ require('./db/data-base');
 
 // defino const express y la requiero, que me la llame
 const express = require('express');
-
+const moviesRouter = require('./router/movies.router');
 const server = express();
-
 //defino el puerto
 const PORT = 3000;
+
+// comienzo a crear enrutado
+
+server.use('/movies', moviesRouter); // le digo al servidor que use la contsante que requiere el archivo que he creado
+
+// cualuqier ruta, que me de el error
+server.use('*', (req, res) => {
+
+    res.status(404).json('Not found')
+});
 
 //arranco el servidor
 server.listen(PORT, () => {
